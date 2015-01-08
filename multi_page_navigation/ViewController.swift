5536a6d1ed845a67aff0e2a1087a0cfc5071a4b4
Stanley Calixte <DuCalixte@gmail.com>
2015-01-08 00:20:01 -0500

    first commit

---
+++ multi_page_navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multi_page_navigation
+//
+//  Created by STANLEY CALIXTE on 1/8/15.
+//  Copyright (c) 2015 STANLEY CALIXTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
